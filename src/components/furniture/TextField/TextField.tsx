153ce2364354cf5820c73e350b9d08f6b0e1910e
hungvt13 <sis.viethung@gmail.com>
2023-11-28 15:39:43 +0700

    feat: added TextField component

---
+++ src/components/furniture/TextField/TextField.tsx
@@ -0,0 +1,9 @@
+import MuiTextField, { TextFieldProps } from "@mui/material/TextField";
+
+function TextField(props : TextFieldProps) {
+  return (
+    <MuiTextField {...props} />
+  );
+}
+
+export default TextField
