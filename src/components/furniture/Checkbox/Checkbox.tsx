fb0bcaba16b1bc8b1188ebc52621d2bb2abeafd3
hungvt13 <sis.viethung@gmail.com>
2023-11-29 17:30:14 +0700

    feat: added Checkbox component

---
+++ src/components/furniture/Checkbox/Checkbox.tsx
@@ -0,0 +1,9 @@
+import MuiCheckbox, { CheckboxProps } from '@mui/material/Checkbox';
+
+function Checkbox(props : CheckboxProps) {
+  return (
+    <MuiCheckbox {...props}/>
+  );
+}
+
+export default Checkbox
