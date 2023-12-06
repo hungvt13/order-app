2998f9d175900bb4345ac48db7148f3b7f87e87c
Hung Tran <sis.viethung@gmail.com>
2023-12-07 02:17:17 +0700

    feature: main menu search (#3)
    * feat: added Select component
    
    * refactor: added memo for MerchantInfo, Image,
    
    * refactor: adjust useScrollToView to accept list of refs
    
    * feat: added intersection hook
    
    * refactor: added memo for functions in useIntersection
    
    * fix: missed use of useCallback with useMemo
    
    * feat: added useDebounce hook
    
    * feat: search function and category scroll to view

---
+++ src/view/MenuPage/useIntersection.js
@@ -0,0 +1,59 @@
+import {
+  useEffect, useRef, useState, useCallback,
+} from 'react';
+
+const useIntersection = (options) => {
+  const targetRefs = useRef([]);
+  const observerRef = useRef(null);
+  const [intersectingIndex, setIntersectingIndex] = useState(null);
+
+  useEffect(() => {
+    observerRef.current = new IntersectionObserver(
+      (entries) => {
+        const intersectingEntry = entries.find((entry) => entry.isIntersecting);
+
+        if (intersectingEntry) {
+          const index = targetRefs.current.indexOf(intersectingEntry.target);
+          setIntersectingIndex(index);
+        } else {
+          setIntersectingIndex(null);
+        }
+      },
+      options,
+    );
+
+    targetRefs.current.forEach((target) => {
+      if (target) {
+        observerRef.current.observe(target);
+      }
+    });
+
+    return () => {
+      targetRefs.current.forEach((target) => {
+        if (target) {
+          observerRef.current.unobserve(target);
+        }
+      });
+    };
+  }, [options]);
+
+  const addTargetRef = useCallback((ref) => {
+    if (ref && !targetRefs.current.includes(ref)) {
+      targetRefs.current.push(ref);
+    }
+  }, []);
+
+  const reset = useCallback(() => {
+    targetRefs.current.forEach((target) => {
+      if (target) {
+        observerRef.current.unobserve(target);
+      }
+    });
+
+    targetRefs.current = [];
+  }, []);
+
+  return [addTargetRef, intersectingIndex, targetRefs, reset];
+};
+
+export default useIntersection;
