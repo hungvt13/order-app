941462a6bc8d7398f5533191af3268ab4509d6dc
hungvt13 <sis.viethung@gmail.com>
2023-12-07 01:21:23 +0700

    feat: added Select component

---
+++ src/components/furniture/MenuItem/MenuItem.tsx
@@ -0,0 +1,9 @@
+import MuiMenuItem, { MenuItemProps } from '@mui/material/MenuItem';
+
+function MenuItem(props : MenuItemProps) {
+  return (
+    <MuiMenuItem {...props} />
+  );
+}
+
+export default MenuItem;
---
+++ src/components/furniture/Select/Select.tsx
@@ -0,0 +1,9 @@
+import MuiSelect, { SelectProps } from '@mui/material/Select';
+
+function Select(props : SelectProps) {
+  return (
+    <MuiSelect {...props} />
+  );
+}
+
+export default Select;
