40a1b256f5c540c488570832f8fa8b573eb0a551
hungvt13 <sis.viethung@gmail.com>
2023-12-22 23:03:54 +0700

    refactor: add get food functions to useCart

--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -1,5 +1,8 @@
+import { useMemo } from 'react';
+
 import { useSelector, useDispatch, shallowEqual } from 'react-redux';
 import { cartActions, cartSelector } from '../state/cart';
+import { merchantSelector } from '../state/merchant';
 
 const useCart = () => {
   const dispatch = useDispatch();
@@ -8,6 +11,14 @@ const useCart = () => {
   const miniCartItems = useSelector(cartSelector.miniCartItems, shallowEqual);
   const cartTotalPrice = useSelector(cartSelector.cartTotalPrice, shallowEqual);
   const cartQuantity = useSelector(cartSelector.cartQuantity, shallowEqual);
+  const merchantFoodList = useSelector(merchantSelector.merchantFoodList, shallowEqual) || [];
+
+  const foodsList = useMemo(
+    () => merchantFoodList
+      .map(({ menu_items: menuItems }) => menuItems)
+      .flat(1),
+    [merchantFoodList],
+  );
 
   const addToCart = (item) => {
     dispatch(cartActions.addToCart(item));
@@ -41,6 +52,46 @@ const useCart = () => {
 
   const getItemByLocalId = (localId) => cartItems[localId];
 
+  const getAddedFoods = (addedItems, originFoodList = merchantFoodList) => {
+    if (addedItems.length === 0 || originFoodList.length === 0) return [];
+
+    // Build a new list by mapping over the keys of addedItems
+    const buildList = Object.keys(addedItems).map((localId) => {
+      // Extract the item details from addedItems using localId
+      const item = { localId, ...addedItems[localId] };
+
+      // Find the corresponding food item in the foods array
+      const sourceFood = originFoodList.find((targetFood) => targetFood.id === item.id);
+
+      // Merge item details with source food details
+      const addedItem = {
+        ...item,
+        ...sourceFood,
+        // Map over the entries of item.options (group ID and option IDs)
+        options: Object.entries(item.options).map(([groupId, optionIds]) => {
+          // Find the corresponding group in the source food options
+          const sourceGroup = sourceFood.options
+            .find((targetGroup) => targetGroup.id.toString() === groupId);
+
+          // Get the option list from the source group
+          const sourceOptions = sourceGroup.option_list;
+
+          // Map over optionIds to get the corresponding options from sourceOptions
+          const mergedOptions = optionIds
+            .map((selectedId) => sourceOptions
+              .find((option) => option.id.toString() === selectedId));
+
+          // Flatten the options array
+          return mergedOptions;
+        }).flat(),
+      };
+
+      return addedItem;
+    });
+
+    return buildList;
+  };
+
   return {
     addToCart,
     addMiniCart,
@@ -48,10 +99,12 @@ const useCart = () => {
     removeMiniCartItems,
     getItemByLocalId,
     updateCart,
+    getAddedFoods,
     cartItems,
     cartTotalPrice,
     cartQuantity,
     miniCartItems,
+    foodsList,
   };
 };
 
